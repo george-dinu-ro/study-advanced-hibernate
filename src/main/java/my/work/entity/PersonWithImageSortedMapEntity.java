d91ce81fbf8b7c8fef9a15014260a349bcbcaa07
george dinu <george.dinu.ro@gmail.com>
2026-07-09 16:48:18 +0300

    add person with image sorted map entity

---
+++ src/main/java/my/work/entity/PersonWithImageSortedMapEntity.java
@@ -0,0 +1,53 @@
+package my.work.entity;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapKeyColumn;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Entity
+@Table(name = "person_with_image_map_tab")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class PersonWithImageSortedMapEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "image_map_tab", joinColumns = @JoinColumn(name = "person_id"))
+    @MapKeyColumn(name = "file_name")
+    @Column(name = "file_description")
+    @OrderBy("file_name desc")
+    private Map<String, String> images = new LinkedHashMap<>();
+
+}
