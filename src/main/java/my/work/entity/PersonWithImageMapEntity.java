b26b135eb033bca75505e15b2f75dc096dc67b13
george dinu <george.dinu.ro@gmail.com>
2026-07-09 15:02:22 +0300

    add person with image map entity

---
+++ src/main/java/my/work/entity/PersonWithImageMapEntity.java
@@ -0,0 +1,51 @@
+package my.work.entity;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapKeyColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "person_with_image_map_tab")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class PersonWithImageMapEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "image_map_tab", joinColumns = @JoinColumn(name = "person_id"))
+    @MapKeyColumn(name = "file_name")
+    @Column(name = "file_description")
+    private List<String> images = new ArrayList<>();
+
+}
