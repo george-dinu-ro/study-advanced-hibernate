e6a77771ca5600ed8b18cfd69d692b259c184ad2
george dinu <george.dinu.ro@gmail.com>
2026-07-09 16:12:57 +0300

    add person with image ordered set service

---
+++ src/main/java/my/work/service/PersonWithImageSortedSetService.java
@@ -0,0 +1,66 @@
+package my.work.service;
+
+import lombok.extern.slf4j.Slf4j;
+import my.work.entity.PersonWithImageSortedSetEntity;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Set;
+
+@Slf4j
+public class PersonWithImageSortedSetService {
+
+    static void main() {
+        try (var sessionFactory = getSessionFactory()) {
+            insertData(sessionFactory);
+            getData(sessionFactory);
+        }
+    }
+
+    private static void insertData(SessionFactory sessionFactory) {
+        try (var session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            getPersons().forEach(personWithImageSetEntity -> {
+                session.persist(personWithImageSetEntity);
+                log.info("Save person {} {}", personWithImageSetEntity.getFirstName(), personWithImageSetEntity.getLastName());
+            });
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private static void getData(SessionFactory sessionFactory) {
+        try (var session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            var person = session
+                    .createNativeQuery("SELECT * FROM person_with_image_set_tab ORDER BY id DESC LIMIT 1", PersonWithImageSortedSetEntity.class)
+                    .getSingleResult();
+
+            log.info("Load person {}", person);
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private static SessionFactory getSessionFactory() {
+        return new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(PersonWithImageSortedSetEntity.class)
+                .buildSessionFactory();
+    }
+
+    private static Set<PersonWithImageSortedSetEntity> getPersons() {
+        return Set.of(
+                PersonWithImageSortedSetEntity.builder()
+                        .firstName("John")
+                        .lastName("Doe")
+                        .email("johndoe@email.com")
+                        .images(Set.of("img1.jpg", "img2.jpg", "img3.jpg"))
+                        .build());
+    }
+
+}
