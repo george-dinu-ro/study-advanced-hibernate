d68ccc4930a4c1f95415b81cb1ca6530a8ea7bd6
george dinu <george.dinu.ro@gmail.com>
2026-07-09 12:17:14 +0300

    add person entity

---
+++ src/main/java/my/work/entity/Person.java
@@ -0,0 +1,49 @@
+package my.work.entity;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "person_tab")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "image_tab", joinColumns = @JoinColumn(name = "person_id"))
+    @Column(name = "file_name")
+    private Set<String> images = new HashSet<>();
+
+}
