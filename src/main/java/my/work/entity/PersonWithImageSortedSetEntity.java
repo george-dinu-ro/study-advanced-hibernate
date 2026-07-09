591f08457bb656b40c0a441f4a514c54223e5f11
george dinu <george.dinu.ro@gmail.com>
2026-07-09 15:47:11 +0300

    add person with image sorted set entity

---
+++ src/main/java/my/work/entity/PersonWithImageSortedSetEntity.java
@@ -0,0 +1,51 @@
+package my.work.entity;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "person_with_image_set_tab")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class PersonWithImageSortedSetEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "image_set_tab", joinColumns = @JoinColumn(name = "person_id"))
+    @Column(name = "file_name")
+    @OrderBy("file_name desc")
+    private Set<String> images = new LinkedHashSet<>();
+
+}
